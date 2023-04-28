3e1d06baeaa1e34a1e353042d96110fd4bbd29bd
Angel André Cotoc Cuzal <78944937+AngelAndreCotocCuzal@users.noreply.github.com>
2023-04-28 12:08:10 -0600

    Subiendo cambios

--- server/conexion_sqlserver.py
+++ server/conexion_sqlserver.py
@@ -2,13 +2,12 @@ import mysql.connector
 
 # Establecemos Coneccion con nuestro host
 # Conn empieza la coneccion
-''
 
 
 def conecciones():
-    #db = mysql.connector.connect(host="localhost", user="root", password="admin123", db="proyecto", port=3306)
+    db = mysql.connector.connect(host="localhost", user="root", password="admin123", db="proyecto", port=3306)
     #db = mysql.connector.connect(host="localhost", user="root", password="dieguito1211", db="proyecto", port=3306)
-    db = mysql.connector.connect(host="192.168.170.249", user="usuario  ", password="url.2023", db="proyecto", port=3306)
+    #db = mysql.connector.connect(host="192.168.170.249", user="usuario  ", password="url.2023", db="proyecto", port=3306)
 
     return db
 
