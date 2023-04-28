9e25f5127f8876111c43e71bc58bfe9616827c3e
diegoabdo <diegoabdoestrada@hotmail.com>
2023-04-28 10:02:53 -0600

    conexion

--- server/conexion_sqlserver.py
+++ server/conexion_sqlserver.py
@@ -6,8 +6,9 @@ import mysql.connector
 
 
 def conecciones():
-
-    db = mysql.connector.connect(host="localhost", user="root", password="admin123", db="proyecto", port=3306)
-    #db = mysql.connector.connect(host="127.0.0.1", user="root", password="2002Febrero%", db="proyecto", port=3306)
+    #db = mysql.connector.connect(host="localhost", user="root", password="admin123", db="proyecto", port=3306)
+    #db = mysql.connector.connect(host="localhost", user="root", password="dieguito1211", db="proyecto", port=3306)
+    db = mysql.connector.connect(host="192.168.170.249", user="usuario  ", password="url.2023", db="proyecto", port=3306)
 
     return db
+
